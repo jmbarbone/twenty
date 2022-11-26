31996a6cb26ca95629a9bdaa3d92c1ebe58acf21
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-11-26 00:51:47 -0500

    improve tests for percentile_rank()

--- tests/testthat/test-percentile.R
+++ tests/testthat/test-percentile.R
@@ -1,21 +1,34 @@
 test_that("percentile_rank()", {
   obj <- percentile_rank(0:9)
-  exp <- c(`0` = 0.05, `1` = 0.15, `2` = 0.25, `3` = 0.35, `4` = 0.45,
-           `5` = 0.55, `6` = 0.65, `7` = 0.75, `8` = 0.85, `9` = 0.95)
+  exp <- new_percentile_rank(
+    c(0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95),
+    0:9
+  )
   expect_equal(obj, exp)
 
-  x <- c(1, 2, 1, 7, 5, NA_integer_, 7, 10)
+  x <- c(1, 2, 1, 7, 5, NA_real_, 7, 10)
   obj <- percentile_rank(x)
-  exp <- structure(
+  exp <- new_percentile_rank(
     c(0.142857142857143, 0.357142857142857, 0.142857142857143, 0.714285714285714, 0.5, NA, 0.714285714285714, 0.928571428571429),
-    names = c("1", "2", "1", "7", "5", NA, "7", "10")
+    x
   )
   expect_equal(obj, exp)
 })
 
 test_that("percentile_rank(weights)", {
   obj <- percentile_rank(7:1, c(1, 0, 2, 2, 3, 1, 1))
-  exp <- c(`7` = 0.95, `6` = 0.9, `5` = 0.8, `4` = 0.6, `3` = 0.35, `2` = 0.15,
-           `1` = 0.05)
+  exp <- new_percentile_rank(c(0.95, 0.9, 0.8, 0.6, 0.35, 0.15, 0.05), 7:1)
   expect_equal(obj, exp)
+})
+
+test_that("percentile_rank() conditions", {
+  expect_error(
+    percentile_rank(c(1, 1, 2), 1),
+    class = "twenty:percentileRankDuplicatedError"
+  )
+
+  expect_error(
+    percentile_rank(1:3, 1:2),
+    class = "twenty:percentileRankLengthsError"
+  )
 })
